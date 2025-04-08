84047cfff80a8cb17803e06aa23d06fadee5ba0e
Daniel <bdcanop@eafit.edu.co>
2025-04-08 14:11:22 -0500

    project initialization

---
+++ src/main/java/com/example/ivanaapi1/Ivanaapi1Application.java
@@ -0,0 +1,13 @@
+package com.example.ivanaapi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ivanaapi1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ivanaapi1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ivanaapi1/Ivanaapi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ivanaapi1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ivanaapi1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
